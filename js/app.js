587714c952f03fc48e9d8feb0d92314843e63047
bw007 <abdulaziz00791@gmail.com>
2023-10-26 07:17:24 +0500

    Flags test

--- js/app.js
+++ js/app.js
@@ -4,22 +4,32 @@ const app = createApp({
   data() {
     return {
       data: [],
+      fullData: [],
     };
   },
   methods: {
-    getCurrencyAll() {
+    getCurrencyAll(flag) {
       axios.get("https://cbu.uz/uz/arkhiv-kursov-valyut/json/")
         .then((res) => {
-          this.data = [...res.data];
-          console.log(this.data[0].Date);
+          
+          this.data = res.data.map(item => {
+            item.flag = this.getCountryFlag()
+            return item;
+          })
+
+          console.log(this.data[0].flag);
         })
         .catch((err) => {
           console.log(err);
         });
     },
+    getCountryFlag(code = 170) {
+      return axios.get(`https://restcountries.com/v3.1/alpha/${code}`)
+        .then((res) => res.data[0].flags.svg)
+    }
   },
   mounted() {
-    this.getCurrencyAll();
+    this.getCurrencyAll()
   },
 });
 
