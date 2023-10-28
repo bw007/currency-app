531c8c42bbd6dbbc204c887d7f2551c440700de1
bw007 <abdulaziz00791@gmail.com>
2023-10-28 12:57:08 +0500

    Test

--- js/app.js
+++ js/app.js
@@ -4,15 +4,13 @@ const app = createApp({
   data() {
     return {
       data: [],
-      fullData: [],
-      // flag: ""
     };
   },
   methods: {
     getCurrencyAll() {
       axios.get("https://cbu.uz/uz/arkhiv-kursov-valyut/json/")
         .then((res) => {
-          this.data = res.data.map(item => item)
+          this.data = res.data.map((item) => (item));
         })
         .catch((err) => {
           console.log(err);
