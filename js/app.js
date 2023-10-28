51d6ce7f1cfccb1044cb01db8dbdeaad5beb64fa
bw007 <abdulaziz00791@gmail.com>
2023-10-28 12:53:56 +0500

    Prepare imgs

--- js/app.js
+++ js/app.js
@@ -5,31 +5,22 @@ const app = createApp({
     return {
       data: [],
       fullData: [],
+      // flag: ""
     };
   },
   methods: {
-    getCurrencyAll(flag) {
+    getCurrencyAll() {
       axios.get("https://cbu.uz/uz/arkhiv-kursov-valyut/json/")
         .then((res) => {
-          
-          this.data = res.data.map(item => {
-            item.flag = this.getCountryFlag()
-            return item;
-          })
-
-          console.log(this.data[0].flag);
+          this.data = res.data.map(item => item)
         })
         .catch((err) => {
           console.log(err);
         });
     },
-    getCountryFlag(code = 170) {
-      return axios.get(`https://restcountries.com/v3.1/alpha/${code}`)
-        .then((res) => res.data[0].flags.svg)
-    }
   },
   mounted() {
-    this.getCurrencyAll()
+    this.getCurrencyAll();
   },
 });
 
